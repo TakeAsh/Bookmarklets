2c36e2191ea7e2d71cf63ffaff68dd6732d9054e
TakeAsh <take@TakeAsh.net>
2022-09-27 03:56:47 +0900

    add showTwEmoji

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Bookmarklets
+
+## Twitter
+
+### showTwEmoji
+- apply Twitter Emoji to current page.
+- [twitter/twemoji](https://github.com/twitter/twemoji)
---
+++ Twitter/showTwEmoji.js
@@ -0,0 +1,11 @@
+﻿javascript:
+((d) => {
+  const style = d.createElement('style');
+  style.appendChild(d.createTextNode('img.emoji { height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -0.1em;}'));
+  d.head.appendChild(style);
+  const script = d.createElement('script');
+  script.src = '//twemoji.maxcdn.com/v/latest/twemoji.min.js';
+  script.crossOrigin = 'anonymous';
+  script.addEventListener('load', () => { twemoji.parse(d.body); });
+  d.head.appendChild(script);
+})(document);
