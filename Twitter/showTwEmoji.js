5275d0bcdf08960597651c49e267aafd1bdfe989
TakeAsh <take@TakeAsh.net>
2022-09-27 04:20:41 +0900

    shorten code

--- Twitter/showTwEmoji.js
+++ Twitter/showTwEmoji.js
@@ -1,7 +1,7 @@
 ﻿javascript:
 ((d) => {
   const style = d.createElement('style');
-  style.appendChild(d.createTextNode('img.emoji { height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -0.1em;}'));
+  style.textContent = 'img.emoji { height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -0.1em;}';
   d.head.appendChild(style);
   const script = d.createElement('script');
   script.src = '//twemoji.maxcdn.com/v/latest/twemoji.min.js';
