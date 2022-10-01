86ab682e6c5c8957e138a408dd6671ded46687e6
TakeAsh <take@TakeAsh.net>
2022-10-02 04:22:38 +0900

    copy title & URL to clipboard

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Bookmarklets
 
+## Title & URL
+
+### copyAsMarkDown
+- copy title and URL of current page to clipboard as markdown.
+- [JavaScript で任意のテキストをクリップボードにコピーする｜まくろぐ](https://maku.blog/p/buk5i2o/)
+
+### copyAsText
+- copy title and URL of current page to clipboard as text.
+- [JavaScript で任意のテキストをクリップボードにコピーする｜まくろぐ](https://maku.blog/p/buk5i2o/)
+
 ## Twitter
 
 ### showTwEmoji
---
+++ Title & URL/copyAsText.js
@@ -0,0 +1,40 @@
+﻿javascript:
+((d) => {
+  const link = `${d.title}\n${location.href}`;
+  const message = copyToClipboard(link) || writeToClipboard(link)
+    ? `Copy to clipbard: ${link}`
+    : `Failed to copy: ${link}`;
+  console.log(message);
+
+  function copyToClipboard(text) {
+    if (!d.execCommand) { return false; } /* execCommand is deprecated */
+    const textarea = d.createElement('textarea');
+    textarea.textContent = text;
+    d.body.appendChild(textarea);
+    textarea.select();
+    d.execCommand('copy');
+    d.body.removeChild(textarea);
+    return true;
+  }
+
+  function writeToClipboard(text) {
+    if (!navigator.clipboard.writeText) { return false; }
+    navigator.permissions.query({ name: 'clipboard-write' })
+      .then((result) => {
+        if (result.state === 'granted' || result.state === 'prompt') {
+          navigator.clipboard.writeText(text)
+            .then(
+              () => { return true; },
+              (err) => {
+                alert('Document is not focued.\nClick document and try again.');
+                return false;
+              }
+            );
+        }
+      })
+      .catch((err) => {
+        /* alert(err); */
+        return false;
+      });
+  }
+})(document);
