9ea6dd0d38c6611ba4d851ce3ac0af3277b4bac1
TakeAsh <take@TakeAsh.net>
2023-01-14 12:50:20 +0900

    Twitter/showTwEmoji: follow Max CDN shut down - [Maxcdn has shut down, cdn not working anymore. · Issue #580 · twitter/twemoji](https://github.com/twitter/twemoji/issues/580)

--- Twitter/showTwEmoji.js
+++ Twitter/showTwEmoji.js
@@ -4,8 +4,8 @@
   style.textContent = 'img.emoji { height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -0.1em; }';
   d.head.appendChild(style);
   const script = d.createElement('script');
-  script.src = '//twemoji.maxcdn.com/v/latest/twemoji.min.js';
+  script.src = 'https://unpkg.com/twemoji@latest/dist/twemoji.min.js';
   script.crossOrigin = 'anonymous';
-  script.addEventListener('load', () => { twemoji.parse(d.body); });
+  script.addEventListener('load', () => { twemoji.parse(d.body, { base: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/' }); });
   d.head.appendChild(script);
 })(document);
