6af5d3ac74b9235062c0026567db0446407d7d9c
TakeAsh <take@TakeAsh.net>
2024-01-06 21:57:59 +0900

    add 'get video ids from playlist'

---
+++ YouTube/getVideoIdsFromPlaylist.js
@@ -0,0 +1,14 @@
+﻿javascript:
+((w, d) => {
+  const ids = Array.from(d.querySelectorAll('a[class~="ytd-playlist-video-renderer"]'))
+    .map(link => link.href.match(/\/watch\?v=(?<videoId>[-_0-9A-Za-z]+)/).groups.videoId);
+  console.log(ids);
+  const textarea = d.createElement('textarea');
+  textarea.value = ids.join('\n');
+  textarea.style.width = '100%';
+  textarea.style.height = '7em';
+  const div = d.querySelector('div[class~="thumbnail-and-metadata-wrapper"]');
+  div.appendChild(textarea);
+  textarea.select();
+  d.execCommand('copy');
+})(window, document);
